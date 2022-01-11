79fee5c5546db3891c9909672a22dd98734cacc0
Simon Jenner <Vinzentjenner@gmail.com>
2022-01-11 23:09:59 +0100

    Remove code

--- android/app/src/main/kotlin/com/example/vinzentjenner_github/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.vinzentjenner_github
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ vinzentjenner_github/lib/src/widgets/jobs.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+import 'profile.dart';
+
+class Job extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
